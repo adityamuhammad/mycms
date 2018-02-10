a538d7ecf56ac1d8588b22baa4d0336415ff0a9d
Muhammad Aditya <aditya210muhammad@gmail.com>
2018-02-10 13:12:14 +0800

    displaying photos to users form

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -7,6 +7,7 @@
     <thead>
       <tr>
         <th>id</th>
+        <th>Photo</th>
         <th>Name</th>
         <th>Email</th>
         <th>Role</th>
@@ -21,9 +22,10 @@
             @foreach($users as $user)
             <tr>
                 <td>{{ $user->id }}</td>
+                <td><img height="50" src="{{ $user->photo ? $user->photo['file'] : 'No Images' }}" alt=""><td>
                 <td>{{ $user->name }}</td>
                 <td>{{ $user->email }}</td>
-                <td>{{ $user->role['name'] == null ? 'User has no role' : $user->role['name'] }}</td>
+                <td>{{ $user->role ? $user->role['name'] : 'User has no role' }}</td>
                 <td>{{ $user->is_active == 1 ? 'Active' : 'Not Active' }}</td>
                 <td>{{ $user->created_at->diffForHumans() }}</td>
                 <td>{{ $user->updated_at->diffForHumans() }}</td>
