a1827e0049148de7839f123a8a098930a9b14e6b
Muhammad Aditya <aditya210muhammad@gmail.com>
2018-02-20 11:34:09 +0800

    modify the front ends, switch colours of navbar

--- resources/views/layouts/blog-post.blade.php
+++ resources/views/layouts/blog-post.blade.php
@@ -28,7 +28,7 @@
 <body>
 
     <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
@@ -53,6 +53,50 @@
                         <a href="#">Contact</a>
                     </li>
                 </ul>
+                <!-- Right Side Of Navbar -->
+                <ul class="nav navbar-nav navbar-right">
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+                    @elseif(Auth::user()->role['name'] == "author")
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/author/home/post') }}"><i class="fa fa-btn glyphicon glyphicon-pencil"></i>Create Post</a></li>
+                                <li><a href="{{ url('/settings/u/profile/') }}"><i class="fa fa-gear fa-fw"></i>Settings</a></li>
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                    @elseif(Auth::user()->role['name'] == "administrator")
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/admin') }}"><i class="fa fa-btn glyphicon glyphicon-user"></i>Go to admin page</a></li>
+                                <li><a href="{{ url('/settings/u/profile/') }}"><i class="fa fa-gear fa-fw"></i>Settings</a></li>
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/settings/u/profile/') }}"><i class="fa fa-gear fa-fw"></i>Settings</a></li>
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+
             </div>
             <!-- /.navbar-collapse -->
         </div>
